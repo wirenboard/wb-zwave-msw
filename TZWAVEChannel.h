e81ff54b5356257313cdfdda0bd5c202db095bb7
Ekaterina Volkova <ekaterina.volkova@wirenboard.com>
2023-02-20 15:04:17 +0300

    Fix formatting

--- TZWAVEChannel.h
+++ TZWAVEChannel.h
@@ -27,13 +27,13 @@ public:
     void ChannelInitialize(String name,
                            TZWAVEChannel::Type type,
                            int32_t errorValue,
-                            uint8_t reportThresHoldParameterNumber,
-                            uint8_t levelSendBasicParameterNumber,
-                            uint8_t hysteresisBasicParameterNumber,
-                            uint8_t onCommandsParameterNumber,
-                            uint8_t offCommandsParameterNumber,
-                            uint8_t onOffCommandsRuleParameterNumber,
-                            uint16_t multiplier,
+                           uint8_t reportThresHoldParameterNumber,
+                           uint8_t levelSendBasicParameterNumber,
+                           uint8_t hysteresisBasicParameterNumber,
+                           uint8_t onCommandsParameterNumber,
+                           uint8_t offCommandsParameterNumber,
+                           uint8_t onOffCommandsRuleParameterNumber,
+                           uint16_t multiplier,
                            TWBMSWSensor* wbMsw,
                            TWBMSWSensor::GetValueCallback readValueCallback,
                            TWBMSWSensor::GetAvailabilityCallback readAvailabilityCallback);
@@ -53,13 +53,34 @@ public:
     uint8_t GetDeviceChannelNumber() const;
     uint8_t GetServerChannelNumber() const;
 
-    inline uint8_t GetReportThresHoldParameterNumber(void) {return (ReportThresHoldParameterNumber);};
-    inline uint8_t GetLevelSendBasicParameterNumber(void) {return (LevelSendBasicParameterNumber);};
-    inline uint8_t GetHysteresisBasicParameterNumber(void) {return (HysteresisBasicParameterNumber);};
-    inline uint8_t GetOnCommandsParameterNumber(void) {return (OnCommandsParameterNumber);};
-    inline uint8_t GetOffCommandsParameterNumber(void) {return (OffCommandsParameterNumber);};
-    inline uint8_t GetOnOffCommandsRuleParameterNumber(void) {return (OnOffCommandsRuleParameterNumber);};
-    inline uint16_t GetMultiplier(void) {return (Multiplier);};
+    inline uint8_t GetReportThresHoldParameterNumber(void)
+    {
+        return (ReportThresHoldParameterNumber);
+    };
+    inline uint8_t GetLevelSendBasicParameterNumber(void)
+    {
+        return (LevelSendBasicParameterNumber);
+    };
+    inline uint8_t GetHysteresisBasicParameterNumber(void)
+    {
+        return (HysteresisBasicParameterNumber);
+    };
+    inline uint8_t GetOnCommandsParameterNumber(void)
+    {
+        return (OnCommandsParameterNumber);
+    };
+    inline uint8_t GetOffCommandsParameterNumber(void)
+    {
+        return (OffCommandsParameterNumber);
+    };
+    inline uint8_t GetOnOffCommandsRuleParameterNumber(void)
+    {
+        return (OnOffCommandsRuleParameterNumber);
+    };
+    inline uint16_t GetMultiplier(void)
+    {
+        return (Multiplier);
+    };
 
     int64_t GetReportedValue() const;
     void SetReportedValue(int64_t reportedValue);
